67471997fa7ce8ff03adadc05f984e2954c042ac
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-11 19:59:52 +0900

    add potato component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Potato />
     </div>
   );
 }

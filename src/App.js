ab76c8e61db3de492138e457774cacbe64894393
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-11 20:35:09 +0900

    id alt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 
 function Food({name, image}){
-  console.log(name);
   return <div>
     <h2>I like {name}</h2>
-    <img src={image} />
+    <img src={image} alt={name} />
   </div>
 }
 
 const foodILike = [
-  {"name":"김치", "image":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fohyena.files.wordpress.com%2F2015%2F02%2Fec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png&f=1&nofb=1"},
-  {"name":"삼겹살", "image":"https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"},
+  {id:1, name:"김치", image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fohyena.files.wordpress.com%2F2015%2F02%2Fec8aa4ed81aceba6b0ec83b7-2015-02-23-21-38-58.png&f=1&nofb=1"},
+  {id:2, name:"삼겹살", image:"https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"},
 ];
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React !</h1>
-      {foodILike.map(dish => <Food name={dish.name} image={dish.image} />)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} image={dish.image} />)}
     </div>
   );
 }

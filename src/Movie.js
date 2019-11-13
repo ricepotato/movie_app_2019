7fdfa7b8df5553af1d859e83f37bc4a73e174315
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-13 20:20:01 +0900

    markup and styling

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>;
+    return <div className="movie">
+        <img src={poster} alt={title} title={title}></img>
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+    </div>;
 }
 
 Movie.propTypes = {

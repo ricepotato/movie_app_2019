d81ec0ea7e815c5647bde8bbdbcedd20761c943a
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-13 20:54:03 +0900

    add css, build

--- src/Movie.js
+++ src/Movie.js
@@ -10,7 +10,7 @@ function Movie({id, year, title, summary, poster, genres}){
         <ul className="genres">
         {genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}</p>
         
     </div>;
 }

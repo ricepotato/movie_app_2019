1062cff685a54d6f425d11a5158c54c03a318262
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-20 19:38:01 +0900

    Updates

---
+++ precache-manifest.33e57d131dfd30a86d2f8b47fa361b47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128e3b9f59c4598a9d6f96d4e63f502a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "c591d86a3ac96bdc1544",
+    "url": "/movie_app_2019/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "4c3f5906623920d7a688",
+    "url": "/movie_app_2019/static/js/2.9a2affb7.chunk.js"
+  },
+  {
+    "revision": "c591d86a3ac96bdc1544",
+    "url": "/movie_app_2019/static/js/main.f67a7891.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
